2f0a41c3e549a9559358758f1aea08255fc6e13c
Ricardo Bejarano <ricardo@tyba.com.co>
2024-04-23 19:42:32 -0500

    fix error

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../feature/counter/counterSlice";
-import shopingCarReducer from "../feature/shoping_car/shopingCarSlice";
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
-    shopingCar: shopingCarReducer,
   },
 });
