dc779d35c195b033ec7255fbafacbb112244543c
Ricardo Bejarano <ricardo@tyba.com.co>
2024-04-23 19:35:00 -0500

    revert

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Counter } from "./feature/counter/";
-import { ShopingCar } from "./feature/shoping_car/";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      {/* <Counter /> */}
-      <ShopingCar />
+      <Counter />
     </div>
   );
 }
